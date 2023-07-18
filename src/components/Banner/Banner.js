21d2dd025ea577396bb8531178cbc2702df38031
leticiaocr <leticiabritooliveirac@gmail.com>
2023-07-18 07:16:35 -0300

    feature: iniciando projeto

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner (){
+    // isso é JSX
+return (
+    <header className='banner'>
+    <img src="/imagens/banner.png" alt="Banner principal da página do organo" />
+    </header>
+)
+}
+
+export default Banner; 
