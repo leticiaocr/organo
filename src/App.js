2c158a137e40328573ca5d0621555ab2d3e61e5d
leticiaocr <leticiabritooliveirac@gmail.com>
2023-07-24 07:00:54 -0300

    feat: iniciando times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -14,7 +15,10 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Front-end" />
+      <Time nome="Data Science" />
     </div>
   );
 }
