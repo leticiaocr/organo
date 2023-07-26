bf9859d73743de3346785f704ae2766597eb99d2
leticiaocr <leticiabritooliveirac@gmail.com>
2023-07-26 07:10:29 -0300

    feature: map colaboradores

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,11 +1,16 @@
+import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = (props) =>{
-return (
-    <section className='time' style={{backgroundColor: props.corSecundaria}} >
-        <h3 style={{borderColor: props.corPrimaria}} >{props.nome}</h3>
-    </section>
-)
+const Time = (props) => {
+    return (
+        <section className='time' style={{ backgroundColor: props.corSecundaria }} >
+            <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+            </div>
+        </section>
+
+    )
 }
 
 export default Time
