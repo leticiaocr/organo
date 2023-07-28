1d2b3c5a933a18785d4aeda18dcce42293bfd1ae
leticiaocr <leticiabritooliveirac@gmail.com>
2023-07-28 07:16:01 -0300

    feature: finalizando projeto

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,17 +1,15 @@
 import './Colaborador.css'
 
-const Colaborador = (props) => {
-    return (
-        <div className='colaborador'>
-            <div className='cabecalho'>
-                <img src={props.imagem} alt={props.nome}/>
-            </div>
-            <div className='rodape'>
-                <h4>{props.nome}</h4>
-                <h5>{props.cargo}</h5>
-            </div>
+const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
+    return (<div className='colaborador'>
+        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+            <img src={imagem} alt={nome}/>
         </div>
-    )
+        <div className='rodape'>
+            <h4>{nome}</h4>
+            <h5>{cargo}</h5>
+        </div>
+    </div>)
 }
 
 export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,12 +3,13 @@ import './time.css'
 
 const Time = (props) => {
     return (
-        <section className='time' style={{ backgroundColor: props.corSecundaria }} >
+        (props.colaboradores.length > 0) ? <section className='time' style={{ backgroundColor: props.corSecundaria }} >
             <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+                {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria}  key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+                
             </div>
-        </section>
+        </section> : ''
 
     )
 }
